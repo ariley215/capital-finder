3e20b04333505b1a0f445e49d01e5c91b89a39fd
Andrea Riley <andreariley@me.com>
2024-01-30 16:41:08 -0500

    variable change

--- api/user.py
+++ api/user.py
@@ -15,23 +15,21 @@ class handler(BaseHTTPRequestHandler):
             response = requests.get(url + dictionary["country"])
             data = response.json()
             country = data[0]
-            capital_city = data["capital"][0] 
+            capital_city = country["capital"][0]
             message = f"The capital of {country['name']['common']} is {capital_city} is"
         elif "capital" in dictionary:
-            response = requests.get(f'https://restcountries.com/v3.1/capital/{dictionary["capital"]}?fields=name,capital')
+            response = requests.get(
+                f'https://restcountries.com/v3.1/capital/{dictionary["capital"]}?fields=name,capital'
+            )
             capitals_response = response.json()
             capital = capitals_response[0]
             capital_country = capital["name"]["common"]
             message = f"{capital} is the capital of {capital_country}"
-            
+
         else:
             message = "What country would you like to know that capital of?"
-            response= {
-              'statusCode': 400,
-              'body': 'Invalid query parameters'
-            }
-            
-            
+            response = {"statusCode": 400, "body": "Invalid query parameters"}
+
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
